e4991c6e02adb9e6dc4bfe9e79d57aff43461439
Yash Kite <yashkite21@gmail.com>
2021-12-27 00:15:39 +0530

    Needs Transaction Given Page button worked successfully

--- app/src/main/java/com/elececo/umoney/Needs.java
+++ app/src/main/java/com/elececo/umoney/Needs.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+
 
 public class Needs extends Fragment {
 
@@ -15,19 +18,34 @@ public class Needs extends Fragment {
         // require a empty public constructor
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_needs, container, false);
+        View rootView = inflater.inflate(R.layout.activity_needs, container,    false);
+        Button needsGivenButton = (Button) rootView.findViewById(R.id.needsGivenButton);
+        Button needsTakenButton = (Button) rootView.findViewById(R.id.needsTakenButton);
+        needsGivenButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                Intent intent = new  Intent(getActivity(), TransactionGivenPage.class);
+                startActivity(intent);
+            }
+
+        });
+        needsTakenButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new  Intent(getActivity(), TransactionTakenPage.class);
+                startActivity(intent);
+            }
+
+        });
+
+        return rootView;
 
     }
-/*
-    public void GivenButtonClick(View view) {
-        Intent intent = new  Intent(getActivity(), TransactionGivenPage.class);
-        startActivity(intent);
-    }
-    public void TakenButtonClick(View view) {
-        Intent intent = new Intent(getActivity(), TransactionTakenPage.class);
-        startActivity(intent);
-    } */
+
+
 }
--- app/src/main/java/com/elececo/umoney/TransactionGivenPage.java
+++ app/src/main/java/com/elececo/umoney/TransactionGivenPage.java
@@ -14,5 +14,6 @@ public class TransactionGivenPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_given_page);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
