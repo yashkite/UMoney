78ea597e40f175e91c2573b2955b72d2f975c869
Yash Kite <yashkite21@gmail.com>
2022-03-18 18:58:07 +0530

    login logout with name working

--- app/src/main/java/com/elececo/umoney/Dashboard.java
+++ app/src/main/java/com/elececo/umoney/Dashboard.java
@@ -1,12 +1,19 @@
 package com.elececo.umoney;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 public class Dashboard extends Fragment {
 
@@ -18,6 +25,23 @@ public class Dashboard extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.activity_dashboard, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(getActivity());
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+            TextView hello = (TextView) getActivity().findViewById(R.id.hello);
+            hello.setText("Hello "+ personName);
+        }
 
     }
 }
