6b8f798d640e805efb0b4b185abb9cd5961cb85f
Yash Kite <32860312+yashkite@users.noreply.github.com>
2022-05-04 23:05:13 +0530

    Fix Given Button bug, also added Files for TransactionUnderApprovalListview & TransactionApproveCard Still unfinished

---
+++ app/src/main/java/com/elececo/umoney/TransactionUnderApprovalListview.java
@@ -0,0 +1,14 @@
+package com.elececo.umoney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TransactionUnderApprovalListview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction_under_approval_listview);
+    }
+}
