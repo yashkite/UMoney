b2123338016fe491c319610237f6f9a7d61b5a3e
Yash Kite <32860312+yashkite@users.noreply.github.com>
2022-05-07 21:24:38 +0530

    dummy data is getting inside and creating list inside TransactionUnderApprovalListview

--- app/src/main/java/com/elececo/umoney/TransactionApproveCard.java
+++ app/src/main/java/com/elececo/umoney/TransactionApproveCard.java
@@ -1,14 +1,49 @@
 package com.elececo.umoney;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class TransactionApproveCard extends AppCompatActivity {
+public class TransactionApproveCard extends ArrayAdapter<String> {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_transaction_approve_card);
+    private final Activity context;
+    private final String[] date;
+    private final String[] paid_to;
+    private final String[] amount;
+
+
+    public TransactionApproveCard (Activity context, String[] date,String[] paid_to, String[] amount) {
+        super(context, R.layout.activity_transaction_approve_card, date);
+        // TODO Auto-generated constructor stub
+
+        this.context=context;
+        this.date=date;
+        this.paid_to=paid_to;
+        this.amount=amount;
+
     }
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.activity_transaction_approve_card, null,true);
+
+        TextView titleText = (TextView) rowView.findViewById(R.id.TA_Date);
+        TextView imageView = (TextView) rowView.findViewById(R.id.TA_PaidTo);
+        TextView subtitleText = (TextView) rowView.findViewById(R.id.TA_Amount);
+
+        titleText.setText(date[position]);
+        imageView.setText(paid_to[position]);
+        subtitleText.setText(amount[position]);
+
+        return rowView;
+
+    };
 }
--- app/src/main/java/com/elececo/umoney/TransactionUnderApprovalListview.java
+++ app/src/main/java/com/elececo/umoney/TransactionUnderApprovalListview.java
@@ -3,12 +3,36 @@ package com.elececo.umoney;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class TransactionUnderApprovalListview extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_transaction_under_approval_listview);
-    }
+
+        ListView list;
+
+        String [] Date ={
+                "01-01-2022","02-01-2022",
+                "03-01-2022","04-01-2022",
+                "05-01-2022",
+        };
+
+        String[] Paid_To ={
+                "A","B",
+                "C","D",
+                "E",
+        };
+
+        String [] Amount={
+                "$ 100", "$ 2000", "$ 5000", "$ 100", "$ 1500"
+        };
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_transaction_under_approval_listview);
+
+            TransactionApproveCard adapter=new TransactionApproveCard(this, Date, Paid_To,Amount);
+            list=(ListView)findViewById(R.id.TA_Listview);
+            list.setAdapter(adapter);
+
+        }
 }
