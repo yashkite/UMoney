047d4f3b92996b2f27f776c618c19f4353c07d55
Yash Kite <32860312+yashkite@users.noreply.github.com>
2022-05-24 06:44:23 +0530

    Changes list
    1. Made entire new card for normal transaction card in activity_transaction_card.xml
    
    2. Combining Given and Taken Transaction page and made a Category and Tags Dropdown spinner
    
    3. Changed Default Text to Numbers in activity_wants.xml
    
    4. AndroidManifest.xml Theme changed to Lite theme
    
    5. Added dependency for FireStore
    
    6. Upgraded build.gradle gradle version
    
    7. strings.xml Added String array for Category and Tags Dropdown spinner
    
    8. Added TransactionCard.java
    
    9. Made Changes for Spinner in TransactionGivenPage.java

--- app/src/main/java/com/elececo/umoney/TransactionGivenPage.java
+++ app/src/main/java/com/elececo/umoney/TransactionGivenPage.java
@@ -1,19 +1,28 @@
 package com.elececo.umoney;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class TransactionGivenPage extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_given_page);
+        Spinner Category = findViewById(R.id.Categories);
+        ArrayAdapter<CharSequence> CategoryAdapter=ArrayAdapter.createFromResource(this, R.array.TransactionCategoryList, android.R.layout.simple_spinner_item);
+        CategoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        Category.setAdapter(CategoryAdapter);
 
-
+        Spinner Tags = findViewById(R.id.Tags);
+        ArrayAdapter<CharSequence> CategoryTags=ArrayAdapter.createFromResource(this, R.array.NeedsTagList, android.R.layout.simple_spinner_item);
+        CategoryTags.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        Tags.setAdapter(CategoryTags);
     }
 
 }
